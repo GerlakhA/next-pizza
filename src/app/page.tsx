4041eb0151468539a5aaf2295c49afb248842165
GerlakhA <andrey215gerlakh@gmail.com>
2024-12-01 14:06:23 +0300

    Add skeleton fro products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useGetProducts } from '@/hooks/useGetProducts'
+import { Skeleton } from '@/shared/components'
 import {
 	Container,
 	Filters,
@@ -13,10 +14,6 @@ import { useCategoryStore } from '@/store/category'
 export default function Home() {
 	const { data: pizzas, isLoading } = useGetProducts()
 	const categoryId = useCategoryStore(state => state.categoryId)
-
-	// if (isLoading) {
-	// 	return <p>Loading...</p>
-	// }
 
 	return (
 		<main>
@@ -32,8 +29,31 @@ export default function Home() {
 					</div>
 					<div className='flex-1'>
 						<div className='flex flex-col gap-16'>
-							<ProductsGroupList title='Пиццы' items={pizzas} categoryId={0} />
-							<ProductsGroupList title='Завтрак' items={pizzas} categoryId={1} />
+							{isLoading ? (
+								<div className={'grid grid-cols-3 gap-[50px]'}>
+									{Array(6)
+										.fill(0)
+										.map((_, i) => (
+											<div key={i} className='p-4'>
+												<div className='flex justify-center p-6 bg-[#F5F5F4]/30 rounded-lg h-[260px]'>
+													<Skeleton className='w-[215px] h-[215px] rounded-full' />
+												</div>
+												<Skeleton className='mb-1 mt-3 rounded-md w-full' />
+												<Skeleton className='rounded-md w-[100px]' />
+												<div className='flex justify-between items-center'>
+													<Skeleton className='w-[85px] h-[30px] rounded-md' />
+
+													<Skeleton className='w-[125px] h-[45px] rounded-md' />
+												</div>
+											</div>
+										))}
+								</div>
+							) : (
+								<>
+									<ProductsGroupList title='Пиццы' items={pizzas} categoryId={0} />
+									<ProductsGroupList title='Завтрак' items={pizzas} categoryId={1} />
+								</>
+							)}
 						</div>
 					</div>
 				</div>
