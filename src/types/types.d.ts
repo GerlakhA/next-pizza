e5f5a0b9fdb5fa8d95cb16cb1807576400de7cfa
GerlakhA <andrey215gerlakh@gmail.com>
2025-04-22 12:14:23 +0300

    delete comments

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -4,7 +4,6 @@ import { type DefaultSession } from 'next-auth'
 declare module 'next-auth' {
 	interface Session {
 		user: {
-			/** The user's postal address. */
 			role: $Enums.UserRole
 			id: string
 		} & DefaultSession['user']
@@ -14,7 +13,6 @@ declare module 'next-auth' {
 import 'next-auth/jwt'
 
 declare module 'next-auth/jwt' {
-	/** Returned by the `jwt` callback and `auth`, when using JWT sessions */
 	interface JWT {
 		role: $Enums.UserRole
 		id: string
