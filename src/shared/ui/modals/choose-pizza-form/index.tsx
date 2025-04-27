b36ad4d3aa658fdb72d077f782052a3194488d2d
GerlakhA <andrey215gerlakh@gmail.com>
2025-04-27 19:15:01 +0300

    fix

--- src/shared/ui/modals/choose-pizza-form/index.tsx
+++ src/shared/ui/modals/choose-pizza-form/index.tsx
@@ -13,10 +13,9 @@ import { PizzaVariants } from '@/shared/ui/pizza-variants'
 type TChoosePizzaForm = {
 	product: ProductWithRelations
 	className?: string
-	onSubmit: () => void
 }
 
-export const ChoosePizzaForm = ({ product, className, onSubmit }: TChoosePizzaForm) => {
+export const ChoosePizzaForm = ({ product, className }: TChoosePizzaForm) => {
 	const {
 		selectedSize,
 		selectedType,
@@ -95,6 +94,8 @@ export const ChoosePizzaForm = ({ product, className, onSubmit }: TChoosePizzaFo
 				</div>
 
 				<Button
+					loading={isPending}
+					onClick={onSubmit}
 					className='h-[55px] px-10 text-base rounded-[18px] w-full mt-10'
 				>
 					Добавить в корзину за {pizzaPrice} ₽
